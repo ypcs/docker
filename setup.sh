27a77db1581e3d09fd0374d57390cb6db137845b
Ville Korhonen <ville@xd.fi>
2018-01-31 10:53:23 +0200

    Cleanup locale directory

--- setup.sh
+++ setup.sh
@@ -55,6 +55,7 @@ cat > /usr/local/sbin/docker-cleanup << EOF
 apt-get --assume-yes autoremove
 apt-get clean
 rm -rf /usr/share/doc/*
+rm -rf /usr/share/locale/*
 rm -rf /usr/share/man/*
 rm -rf /var/lib/apt/lists/* /var/cache/apt/*.bin
 mkdir -p /var/lib/apt/lists/partial
