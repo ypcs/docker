7ac386487aaf1397de3a9458168964da4160b4c6
Ville Korhonen <ville@xd.fi>
2018-02-15 23:15:14 +0200

    Add support for init scripts

--- images/php/entrypoint.sh
+++ images/php/entrypoint.sh
@@ -6,5 +6,26 @@ if [ "${1#-}" != "$1" ]; then
 	set -- php-fpm${PHP_VERSION:-7.0} "$@"
 fi
 
+if [ ! -e "/initialized" ]
+then
+    if [ -d "/docker-entrypoint-init.d" ]
+    then
+        for f in /docker-entrypoint-init.d/*
+        do
+            case "${f}"
+            in
+                *.sh)
+                    echo "$0: running $f"
+                    . "$f"
+                    ;;
+                *)
+                   echo "$0: ignoring $f"
+                   ;;
+            esac
+        done
+
+    fi
+fi
+
 exec "$@"
 
