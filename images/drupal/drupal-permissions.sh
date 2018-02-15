45259c594087660b20324a1384705ba0104ceed6
Ville Korhonen <ville@xd.fi>
2018-02-15 23:37:34 +0200

    Fix permissons: set www-data as group

--- images/drupal/drupal-permissions.sh
+++ images/drupal/drupal-permissions.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e
 
-find /var/www/html -type d -exec chmod 0750 '{}' \; && \
-find /var/www/html -type f -exec chmod 0640 '{}' \; && \
-find /var/www/html/sites -type d -name files -exec chmod 0770 '{}' \; && \
-find /var/www/html/sites -type d -path '*/files/*' -exec chmod 0770 '{}' \; && \
+find /var/www/html -exec chown root:www-data '{}' \;
+find /var/www/html -type d -exec chmod 0755 '{}' \;
+find /var/www/html -type f -exec chmod 0644 '{}' \;
+find /var/www/html/sites -type d -name files -exec chmod 0770 '{}' \;
+find /var/www/html/sites -type d -path '*/files/*' -exec chmod 0770 '{}' \;
 find /var/www/html/sites -type f -path '*/files/*' -exec chmod 0660 '{}' \;
 
