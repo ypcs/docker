40418e14578d7c2add4fee1a66a92467aa769b04
Ville Korhonen <ville@xd.fi>
2018-02-16 00:13:54 +0200

    Add some output messages to log

--- images/drupal/drupal-permissions.sh
+++ images/drupal/drupal-permissions.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 set -e
 
-find /var/www/html -exec chown root:www-data '{}' \;
-find /var/www/html -type d -exec chmod 0755 '{}' \;
-find /var/www/html -type f -exec chmod 0644 '{}' \;
-find /var/www/html/sites -type d -name files -exec chmod 0770 '{}' \;
-find /var/www/html/sites -type d -path '*/files/*' -exec chmod 0770 '{}' \;
-find /var/www/html/sites -type f -path '*/files/*' -exec chmod 0660 '{}' \;
+DOCUMENT_ROOT="${DOCUMENT_ROOT:-/var/www/html}"
+
+echo "Change file ownership to root:www-data."
+find "${DOCUMENT_ROOT}" -exec chown root:www-data '{}' \;
+
+echo "Change file permissions (d: 0755, f: 0644)."
+find "${DOCUMENT_ROOT}" -type d -exec chmod 0755 '{}' \;
+find "${DOCUMENT_ROOT}" -type f -exec chmod 0644 '{}' \;
+
+echo "Add write permissions to user content."
+find "${DOCUMENT_ROOT}/sites" -type d -name files -exec chmod 0770 '{}' \;
+find "${DOCUMENT_ROOT}/sites" -type d -path '*/files/*' -exec chmod 0770 '{}' \;
+find "${DOCUMENT_ROOT}/sites" -type f -path '*/files/*' -exec chmod 0660 '{}' \;
 
