c287343b1dca812df700853165df2613c91b7c0b
Ville Korhonen <ville@xd.fi>
2018-02-20 22:09:29 +0200

    Fix variable names

--- images/drupal/drupal-setup.sh
+++ images/drupal/drupal-setup.sh
@@ -4,10 +4,10 @@ set -e
 DRUPAL_SITE="${DRUPAL_SITE:-Example Site}"
 DRUPAL_USERNAME="${DRUPAL_USERNAME:-admin}"
 DRUPAL_PASSWORD="${DRUPAL_PASSWORD:-admin}"
-DRUPAL_DB_HOST="${DRUPAL_DB_HOST:-db}"
-DRUPAL_DB="${DRUPAL_DB:-drupal}"
-DRUPAL_DB_USER="${DRUPAL_DB_USER:-drupal}"
-DRUPAL_DB_PASSWORD="${DRUPAL_DB_PASSWORD:-drupal}"
+DRUPAL_DATABASE_HOST="${DRUPAL_DATABASE_HOST:-db}"
+DRUPAL_DATABASE_NAME="${DRUPAL_DATABASE_NAME:-drupal}"
+DRUPAL_DATABASE_USER="${DRUPAL_DATABASE_USER:-drupal}"
+DRUPAL_DATABASE_PASSWORD="${DRUPAL_DATABASE_PASSWORD:-drupal}"
 
 DRUPAL_ROOT="${DRUPAL_ROOT:-${DOCUMENT_ROOT:-/var/www/html}}"
 
@@ -27,7 +27,7 @@ set -e
 if [ "${STATUS}" = "1" ]
 then
     drush site-install standard -y \
-        --db-url="mysql://${DRUPAL_DB_USER}:${DRUPAL_DB_PASSWORD}@${DRUPAL_DB_HOST}/${DRUPAL_DB}" \
+        --db-url="mysql://${DRUPAL_DATABASE_USER}:${DRUPAL_DATABASE_PASSWORD}@${DRUPAL_DATABASE_HOST}/${DRUPAL_DATABASE_NAME}" \
         --site-name="${DRUPAL_SITE}" \
         --account-name="${DRUPAL_USERNAME}" \
         --account-pass="${DRUPAL_PASSWORD}"
