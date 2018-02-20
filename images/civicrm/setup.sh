eaddcefa16ff428ed852d961794ad6f0660031f8
Ville Korhonen <ville@xd.fi>
2018-02-20 12:58:54 +0200

    Add support for Drupal login

--- images/civicrm/setup.sh
+++ images/civicrm/setup.sh
@@ -8,14 +8,30 @@ CIVICRM_DATABASE_NAME="${CIVICRM_DATABASE_NAME:-civicrm}"
 
 CIVICRM_INSTALL_URL="${CIVICRM_INSTALL_URL:-http://web/sites/all/modules/civicrm/install/index.php}"
 
+DRUPAL_USERNAME="${DRUPAL_USERNAME:-admin}"
+DRUPAL_PASSWORD="${DRUPAL_PASSWORD:-admin}"
+DRUPAL_LOGIN_URL="${DRUPAL_LOGIN_URL:-http://web/user/login}"
+
 DRUPAL_DATABASE_HOST="${DRUPAL_DATABASE_HOST:-db}"
 DRUPAL_DATABASE_USER="${DRUPAL_DATABASE_USER:-drupal}"
 DRUPAL_DATABASE_PASSWORD="${DRUPAL_DATABASE_PASSWORD:-drupal}"
 DRUPAL_DATABASE_NAME="${DRUPAL_DATABASE_NAME:-drupal}"
 
+TEMPFILE="$(mktemp)"
+
+echo "Login to Drupal"
+curl \
+    --cookie-jar "${TEMPFILE}" \
+    -X POST \
+    -F "name=${DRUPAL_USERNAME}" \
+    -F "pass=${DRUPAL_PASSWORD}" \
+    -F "form_id=user_login" \
+    -F "op=Log in" \
+    "${DRUPAL_LOGIN_URL}"
 
 echo "Submit CiviCRM install request"
 curl \
+    --cookie-jar "${TEMPFILE}" \
     -X POST \
     -F "mysql[server]=${CIVICRM_DATABASE_HOST}" \
     -F "mysql[user]=${CIVICRM_DATABASE_USER}" \
@@ -29,3 +45,5 @@ curl \
     "${CIVICRM_INSTALL_URL}"
 
 # force_reinstall=1
+
+rm -f "${TEMPFILE}"
