7482376a3b2abeb3cc1aea36a0c5b02d9b2d5c76
Ville Korhonen <ville@xd.fi>
2018-02-20 13:07:40 +0200

    Make cURL silent again

--- images/civicrm/setup.sh
+++ images/civicrm/setup.sh
@@ -22,16 +22,18 @@ TEMPFILE="$(mktemp)"
 echo "Login to Drupal"
 curl \
     --cookie-jar "${TEMPFILE}" \
+    --silent \
     -X POST \
     -F "name=${DRUPAL_USERNAME}" \
     -F "pass=${DRUPAL_PASSWORD}" \
     -F "form_id=user_login" \
     -F "op=Log in" \
-    "${DRUPAL_LOGIN_URL}"
+    "${DRUPAL_LOGIN_URL}" 1>/dev/null
 
 echo "Submit CiviCRM install request"
 curl \
     --cookie "${TEMPFILE}" \
+    --silent \
     -X POST \
     -F "mysql[server]=${CIVICRM_DATABASE_HOST}" \
     -F "mysql[username]=${CIVICRM_DATABASE_USER}" \
@@ -44,7 +46,7 @@ curl \
     -F "go=1" \
     -F "seedLanguage=en_US" \
     -F "loadGenerated=0" \
-    "${CIVICRM_INSTALL_URL}"
+    "${CIVICRM_INSTALL_URL}" 1>/dev/null
 
 # force_reinstall=1
 
