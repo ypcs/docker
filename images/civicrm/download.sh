78c53cf0737fcddf3b6dafb40199c0fbc0a70f59
Ville Korhonen <ville@xd.fi>
2018-02-20 10:34:34 +0200

    Download CiviCRM l10n files

--- images/civicrm/download.sh
+++ images/civicrm/download.sh
@@ -11,11 +11,13 @@ TEMPDIR="$(realpath "$(mktemp -d)")"
 cd "${TEMPDIR}"
 
 curl -fSL "https://storage.googleapis.com/civicrm/civicrm-stable/${CIVICRM_VERSION}/civicrm-${CIVICRM_VERSION}-drupal.tar.gz" -o "civicrm.tar.gz"
+curl -fSL "https://storage.googleapis.com/civicrm/civicrm-stable/${CIVICRM_VERSION}/civicrm-${CIVICRM_VERSION}-l10n.tar.gz" -o "civicrm-l10n.tar.gz"
 
 echo "${CIVICRM_SHA256} *civicrm.tar.gz" |sha256sum -c -
 
 cd "${DRUPAL_MODULE_DIRECTORY}"
 tar xzf "${TEMPDIR}/civicrm.tar.gz"
+tar xzf "${TEMPDIR}/civicrm-l10n.tar.gz"
 
 echo "Installed CiviCRM ${CIVICRM_VERSION} to ${DRUPAL_MODULE_DIRECTORY}."
 
