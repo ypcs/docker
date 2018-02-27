2b9810990bb626b3a2e0c17f60bdbde044faafb8
Ville Korhonen <ville@xd.fi>
2018-02-27 19:05:05 +0200

    Add environment-based configuration

---
+++ images/civicrm/civicrm.development.settings.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * CiviCRM settigns for development environment
+ *
+ * These are based on staging, which in turn is based on production
+ * settings.
+ */
+require_once("civicrm.staging.settings.php");
+
+$civicrm_setting['CiviCRM Preferences']['enableSSL'] = 0;
